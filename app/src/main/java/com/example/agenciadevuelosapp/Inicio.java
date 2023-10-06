020a35426cb08dd278cdd3af963f5b799daa3380
Kevin <kevin.hernandez7@catolica.edu.sv>
2023-10-05 21:02:06 -0600

    Hice otro textview :)

--- app/src/main/java/com/example/agenciadevuelosapp/Inicio.java
+++ app/src/main/java/com/example/agenciadevuelosapp/Inicio.java
@@ -3,9 +3,11 @@ package com.example.agenciadevuelosapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Inicio extends AppCompatActivity {
 
+    TextView Rodrigo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
