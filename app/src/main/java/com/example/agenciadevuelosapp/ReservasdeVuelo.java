6bc9d256731c3d1d64333a53440a3bae13944891
Frankleen <frankleen.leiva@catolica.edu.sv>
2023-10-06 12:07:13 -0600

    cree un activity

---
+++ app/src/main/java/com/example/agenciadevuelosapp/ReservasdeVuelo.java
@@ -0,0 +1,14 @@
+package com.example.agenciadevuelosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReservasdeVuelo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservasde_vuelo);
+    }
+}
