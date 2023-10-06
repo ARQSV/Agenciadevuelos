29f30170789fb237cdd506c629e3b6c874cc92cb
LudwinCst <ludwin.castillo@catolica.edu.sv>
2023-10-06 17:03:24 -0600

    Avance, Login y Registrarse

--- app/src/main/java/com/example/agenciadevuelosapp/Login.java
+++ app/src/main/java/com/example/agenciadevuelosapp/Login.java
@@ -1,19 +1,54 @@
 package com.example.agenciadevuelosapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    private EditText editTextEmail, editTextPassword;
+    private Button loginButton;
+    private TextView registerTextView;
 
-    ImageView login;
+    private static final String USUARIO_PRED = "admin123@gmail.com";
+    private static final String CONTRASEÑA_PRED = "admin123";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        editTextEmail = findViewById(R.id.editTextemail);
+        editTextPassword = findViewById(R.id.editTextpassword);
+        loginButton = findViewById(R.id.registerButton);
+        registerTextView = findViewById(R.id.loginTextView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (email.equals(USUARIO_PRED) && password.equals(CONTRASEÑA_PRED)) {
+                    Intent intent = new Intent(Login.this, Cards.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(Login.this, "Sus credenciales no coinciden", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Registrarse.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/agenciadevuelosapp/Registrarse.java
+++ app/src/main/java/com/example/agenciadevuelosapp/Registrarse.java
@@ -1,14 +1,52 @@
 package com.example.agenciadevuelosapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class Registrarse extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextUsername, editTextPassword;
+    private Button registerButton;
+    private TextView loginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+
+        editTextEmail = findViewById(R.id.editTextemail);
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextpassword);
+        registerButton = findViewById(R.id.registerButton);
+        loginTextView = findViewById(R.id.loginTextView);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString().trim();
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (email.isEmpty() || username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(Registrarse.this, "Todos los campos son requeridos", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Registrarse.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Registrarse.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
