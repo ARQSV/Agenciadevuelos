dd819fd27e6811d26d35bc923d1d5d311873f64e
Kevin <kevin.hernandez7@catolica.edu.sv>
2023-10-08 22:50:41 -0600

    Añadi las demas vistas

--- app/src/main/java/com/example/agenciadevuelosapp/Cards.java
+++ app/src/main/java/com/example/agenciadevuelosapp/Cards.java
@@ -1,8 +1,11 @@
 package com.example.agenciadevuelosapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Cards extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class Cards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
+
+        CardView cardViewElSalvador = findViewById(R.id.cardView3);
+        CardView cardViewFrancia = findViewById(R.id.cardView4);
+        CardView cardViewChina = findViewById(R.id.cardView5);
+
+        cardViewElSalvador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cards.this, elsalvador.class);
+                startActivity(intent);
+            }
+        });
+
+        cardViewFrancia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cards.this, francia.class);
+                startActivity(intent);
+            }
+        });
+
+        cardViewChina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Cards.this, China.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
