07bb739e9eb031b59f12bcf59617cc561ac913f4
LudwinCst <ludwin.castillo@catolica.edu.sv>
2023-10-09 23:23:19 -0600

    Conexion

--- app/src/main/java/com/example/agenciadevuelosapp/Cards.java
+++ app/src/main/java/com/example/agenciadevuelosapp/Cards.java
@@ -2,7 +2,9 @@ package com.example.agenciadevuelosapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Cards extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Cards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
+    }
+
+    public void Reserva(View view) {
+        Intent reserva = new Intent(this, ReservasdeVuelo.class);
+        startActivity(reserva);
     }
 }
