516ce33240f9088ea28e681419ee1c765b5d7fff
LudwinCst <ludwin.castillo@catolica.edu.sv>
2023-10-11 10:20:40 -0600

    AVANCE

---
+++ app/src/main/java/com/example/agenciadevuelosapp/ItemModel.java
@@ -0,0 +1,19 @@
+package com.example.agenciadevuelosapp;
+
+public class ItemModel {
+
+    private String id;
+
+    String title;
+    String desc;
+
+    public ItemModel(String id, String title, String desc) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ app/src/main/java/com/example/agenciadevuelosapp/Reservados.java
@@ -0,0 +1,13 @@
+package com.example.agenciadevuelosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reservados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservados);
+    }
+}
