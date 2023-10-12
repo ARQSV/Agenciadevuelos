a8d2c8f56f892b14c84e8f022b199be7476b1192
Frankleen <frankleen.leiva@catolica.edu.sv>
2023-10-11 22:21:45 -0600

    avanze3

---
+++ app/src/main/java/com/example/agenciadevuelosapp/customPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.agenciadevuelosapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class customPagerAdapter extends PagerAdapter {
+    private Context context;
+    private int[] images;
+
+    public customPagerAdapter(Context context, int[] images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.pager_item, container, false);
+
+        ImageView imageView = itemView.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
+
+
+
